f430b975e8d04427fc06120c0f73c6f2c6537747
RomeroGabriel <gabrielromerods@hotmail.com>
2019-05-22 01:47:24 -0300

    Regras de coordenador começando

---
+++ src/main/java/utfpr/arquitetura1/antecipacao/rules/CoordinatorRules.java
@@ -0,0 +1,16 @@
+package utfpr.arquitetura1.antecipacao.rules;
+
+import org.springframework.beans.BeanUtils;
+import utfpr.arquitetura1.antecipacao.DAO.SolicitationDAO;
+import utfpr.arquitetura1.antecipacao.DTO.SolicitationDTO;
+import utfpr.arquitetura1.antecipacao.Entity.SolicitationEntity;
+import utfpr.arquitetura1.antecipacao.enums.SolicitationStatus;
+
+public class CoordinatorRules {
+
+    public void acceptAnticipation(SolicitationDTO solicitation) {
+        solicitation.setStatus(SolicitationStatus.APPROVED);
+        SolicitationEntity solicitationUpdate = new SolicitationEntity();
+        BeanUtils.copyProperties(solicitationUpdate, solicitation);
+    }
+}
