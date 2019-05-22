5885575d9c8d0b38ac2c93405b95c6f9c8c23335
renanbatel <renanbatel@gmail.com>
2019-05-22 02:49:43 -0300

    AnticipationRules updates

--- src/main/java/utfpr/arquitetura1/antecipacao/rules/AnticipationRules.java
+++ src/main/java/utfpr/arquitetura1/antecipacao/rules/AnticipationRules.java
@@ -1,27 +1,40 @@
 package utfpr.arquitetura1.antecipacao.rules;
 
 import org.springframework.beans.BeanUtils;
+import utfpr.arquitetura1.antecipacao.DAO.AcademicCalendarDAO;
 import utfpr.arquitetura1.antecipacao.DAO.AnticipationDAO;
 import utfpr.arquitetura1.antecipacao.DTO.AnticipationDTO;
+import utfpr.arquitetura1.antecipacao.Entity.AcademicCalendarEntity;
 import utfpr.arquitetura1.antecipacao.Entity.AnticipationEntity;
 import utfpr.arquitetura1.antecipacao.exceptions.InvalidAnticipationDateException;
 
 public class AnticipationRules {
 
     private AnticipationDAO anticipationDAO;
+    AcademicCalendarDAO academicCalendarDAO;
 
-    private Boolean anticipationDateIsBeforeLessonDate( AnticipationDTO anticipation ) {
+    private Boolean anticipationDateIsBeforeLessonDate(AnticipationDTO anticipation) {
 
         return anticipation.getDate().compareTo(anticipation.getLesson().getDate()) < 0;
     }
 
-    public void insert(AnticipationDTO anticipation) throws InvalidAnticipationDateException {
+    private Boolean anticipationRespectsTheAcademicCalendar(AnticipationDTO anticipation) {
+        AcademicCalendarEntity academicCalendar = this.academicCalendarDAO.getOne(new Long(1));
+
+        return anticipation.getDate().compareTo(academicCalendar.getStartDate()) > 0
+            && anticipation.getDate().compareTo(academicCalendar.getFinishDate()) < 0;
+    }
+
+    public AnticipationEntity insert(AnticipationDTO anticipation) throws InvalidAnticipationDateException {
         if (!this.anticipationDateIsBeforeLessonDate(anticipation))
-            throw new InvalidAnticipationDateException();
+            throw new InvalidAnticipationDateException("The anticipation cannot be scheduled after its class");
+        if (!this.anticipationRespectsTheAcademicCalendar(anticipation))
+            throw new InvalidAnticipationDateException("The anticipation date must respect the academic calendar");
 
         AnticipationEntity entity = new AnticipationEntity();
 
         BeanUtils.copyProperties(anticipation, entity);
-        anticipationDAO.save(entity);
+
+        return this.anticipationDAO.save(entity);
     }
 }
--- src/test/java/utfpr/arquitetura1/antecipacao/rules/AnticipationRulesTest.java
+++ src/test/java/utfpr/arquitetura1/antecipacao/rules/AnticipationRulesTest.java
@@ -1,12 +1,18 @@
 package utfpr.arquitetura1.antecipacao.rules;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import utfpr.arquitetura1.antecipacao.DAO.AcademicCalendarDAO;
+import utfpr.arquitetura1.antecipacao.DTO.AcademicCalendarDTO;
 import utfpr.arquitetura1.antecipacao.DTO.AnticipationDTO;
 import utfpr.arquitetura1.antecipacao.DTO.LessonDTO;
+import utfpr.arquitetura1.antecipacao.Entity.AcademicCalendarEntity;
 import utfpr.arquitetura1.antecipacao.exceptions.InvalidAnticipationDateException;
 
 import java.text.ParseException;
@@ -17,14 +23,26 @@ import java.text.SimpleDateFormat;
 public class AnticipationRulesTest {
 
     AnticipationRules anticipationRules;
+    @Autowired
+    AcademicCalendarDAO academicCalendarDAO;
 
     @Before
-    public void before() {
+    public void before() throws ParseException {
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
         this.anticipationRules = new AnticipationRules();
+        this.academicCalendarDAO.save(
+                AcademicCalendarEntity
+                        .builder()
+                        .startDate(format.parse("02/03/2019 00:00:00"))
+                        .finishDate(format.parse("07/07/2019 00:00:00"))
+                        .semester(1)
+                        .build()
+        );
     }
 
     @Test (expected = InvalidAnticipationDateException.class)
-    public void anticipationIsNotBeforeLesson() throws InvalidAnticipationDateException, ParseException {
+    public void anticipationIsNotBeforeLesson() throws ParseException {
         SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
         LessonDTO lesson = LessonDTO
                 .builder()
@@ -36,6 +54,11 @@ public class AnticipationRulesTest {
                 .lesson(lesson)
                 .build();
 
-        this.anticipationRules.insert(anticipation);
+        try {
+            this.anticipationRules.insert(anticipation);
+        } catch (InvalidAnticipationDateException e) {
+            Assert.assertEquals("The anticipation cannot be scheduled after its class", e.getMessage());
+        }
     }
+
 }
