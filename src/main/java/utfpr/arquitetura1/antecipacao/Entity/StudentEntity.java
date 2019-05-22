cb238f15a645a5921a979c0e47d1455dcf0b5667
RomeroGabriel <gabrielromerods@hotmail.com>
2019-05-22 01:14:08 -0300

    Ajustes Romero

--- src/main/java/utfpr/arquitetura1/antecipacao/Entity/StudentEntity.java
+++ src/main/java/utfpr/arquitetura1/antecipacao/Entity/StudentEntity.java
@@ -6,7 +6,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
-import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import java.io.Serializable;
 import java.util.Set;
